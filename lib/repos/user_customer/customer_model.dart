4575d87f2e8f6e567dc08e9d79c2c1c8f87ecd09
Gayangi Seneviratne <sgayangi@gmail.com>
2021-08-17 16:10:59 +0530

    customer model updated

--- lib/repos/user_customer/customer_model.dart
+++ lib/repos/user_customer/customer_model.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Customer {
-  String? name;
+  String? firstName;
+  String? lastName;
   DateTime? birthDate;
-  List<Card>? cards = [];
+  // List<Card>? cards = [];
   String? email;
   String? password;
+  String? city;
+  String country;
+
+  Customer({
+    this.firstName,
+    this.lastName,
+    this.birthDate,
+    // this.cards,
+    this.email,
+    this.password,
+    this.city,
+    this.country = "Sri Lanka",
+  });
 }
