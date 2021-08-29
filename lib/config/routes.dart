a283df766c3da26b80535d4851fc3b2b121f2252
Gayangi Seneviratne <sgayangi@gmail.com>
2021-08-29 17:15:24 +0530

    routes updated to include orders page

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -4,8 +4,8 @@ import 'package:go_pharma/ui/customer/checkout/checkout_receipt.dart';
 import 'package:go_pharma/ui/customer/checkout/checkout_start.dart';
 import 'package:go_pharma/ui/customer/customer_starting_page.dart';
 import 'package:go_pharma/ui/customer/home/customer_home_page.dart';
+import 'package:go_pharma/ui/customer/past_orders/past_orders_page.dart';
 import 'package:go_pharma/ui/customer/products/product_categories/categories_routing_page.dart';
-import 'package:go_pharma/ui/customer/products/product_categories/specific_category_page.dart';
 import 'package:go_pharma/ui/customer/products/product_home_page.dart';
 import 'package:go_pharma/ui/customer/checkout/shopping_cart_page.dart';
 import 'package:go_pharma/ui/customer/profile/settings.dart';
@@ -36,6 +36,7 @@ Map<String, Widget Function(BuildContext context)> routes = {
   CheckoutReceipt.id: (context) => CheckoutReceipt(),
   ProductCategoriesPage.id: (context) => ProductCategoriesPage(),
   SearchPage.id: (context) => SearchPage(),
+  PastOrdersPage.id: (context) => PastOrdersPage(),
 
   //delivery agent pages
   DeliveryAgentStartingPage.id: (context) => DeliveryAgentStartingPage(),
