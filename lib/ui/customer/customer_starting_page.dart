2ebc4fdfa263787f790f2c13ac2ded50775ea5eb
Gayangi Seneviratne <sgayangi@gmail.com>
2021-09-01 22:08:15 +0530

    home page and starting page of customer refactored

--- lib/ui/customer/customer_starting_page.dart
+++ lib/ui/customer/customer_starting_page.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_pharma/bloc/customer/camera/camera_bloc.dart';
-import 'package:go_pharma/bloc/customer/checkout/checkout_bloc.dart';
 import 'package:go_pharma/ui/common/colors.dart';
 import 'package:go_pharma/ui/common/widgets/rounded_button.dart';
-import 'package:go_pharma/ui/customer/settings/settings.dart';
 import 'package:go_pharma/ui/customer/sign_in/customer_sign_in.dart';
 import 'package:go_pharma/ui/customer/sign_up/customer_sign_up.dart';
-import 'camera/select_photo_screen.dart';
-import 'home/customer_home_page.dart';
 
 class CustomerStartingPage extends StatelessWidget {
   static const String id = "customer_starting_page";
@@ -18,77 +12,37 @@ class CustomerStartingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CheckoutBloc>(
-          create: (_) => CheckoutBloc(_),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to GoPharma!"),
         ),
-        BlocProvider<CameraBloc>(
-          create: (_) => CameraBloc(_),
-        ),
-      ],
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Welcome to GoPharma!"),
-          ),
-          body: Container(
-            width: size.width,
-            height: size.height,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RoundedButtonFilled(
-                  title: "LOGIN",
-                  size: size,
-                  onTapped: () => Navigator.pushNamed(
-                    context,
-                    CustomerSignInStart.id,
-                  ),
+        body: Container(
+          width: size.width,
+          height: size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RoundedButtonFilled(
+                title: "LOGIN",
+                size: size,
+                onTapped: () => Navigator.pushNamed(
+                  context,
+                  CustomerSignInStart.id,
                 ),
-                RoundedButtonFilled(
-                  title: "SIGN UP",
-                  size: size,
-                  fillColor: GoPharmaColors.GreyColor.withOpacity(0.5),
-                  textColor: GoPharmaColors.BlackColor,
-                  onTapped: () => Navigator.pushNamed(
-                    context,
-                    CustomerSignUpStart.id,
-                  ),
+              ),
+              RoundedButtonFilled(
+                title: "SIGN UP",
+                size: size,
+                fillColor: GoPharmaColors.GreyColor.withOpacity(0.5),
+                textColor: GoPharmaColors.BlackColor,
+                onTapped: () => Navigator.pushNamed(
+                  context,
+                  CustomerSignUpStart.id,
                 ),
-                RoundedButtonFilled(
-                  title: "CUSTOMER HOME PAGE",
-                  size: size,
-                  fillColor: GoPharmaColors.GreyColor.withOpacity(0.5),
-                  textColor: GoPharmaColors.BlackColor,
-                  onTapped: () => Navigator.pushNamed(
-                    context,
-                    CustomerHomePage.id,
-                  ),
-                ),
-                RoundedButtonFilled(
-                  title: "CAMERA SCREEN",
-                  size: size,
-                  fillColor: GoPharmaColors.GreyColor.withOpacity(0.5),
-                  textColor: GoPharmaColors.BlackColor,
-                  onTapped: () => Navigator.pushNamed(
-                    context,
-                    SelectPhotoScreen.id,
-                  ),
-                ),
-                RoundedButtonFilled(
-                  title: "SETTINGS PAGE",
-                  size: size,
-                  fillColor: GoPharmaColors.GreyColor.withOpacity(0.5),
-                  textColor: GoPharmaColors.BlackColor,
-                  onTapped: () => Navigator.pushNamed(
-                    context,
-                    SettingsPage.id,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
