507478d2a64a6bf76a77ee975595da20c539b8b4
Gayangi Seneviratne <sgayangi@gmail.com>
2021-09-01 22:07:34 +0530

    product action and images put into separate files

---
+++ lib/ui/customer/products/product_action_icon.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:go_pharma/ui/common/colors.dart';
+
+class ProductActionIcon extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const ProductActionIcon({
+    Key? key,
+    required this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      child: FittedBox(
+        child: FloatingActionButton(
+          heroTag: Random().nextInt(100000).toString(),
+          child: Icon(
+            this.icon,
+            color: Colors.black87,
+          ),
+          backgroundColor: GoPharmaColors.GreyColor,
+          onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
