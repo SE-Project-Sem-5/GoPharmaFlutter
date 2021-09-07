f927900c77c5be1ab90beded9a100ac571005665
Gayangi Seneviratne <sgayangi@gmail.com>
2021-09-07 14:52:38 +0530

    delivery full view added

---
+++ lib/ui/delivery_agent/delivery/delivery_full_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliveryFullView extends StatelessWidget {
+  const DeliveryFullView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
