905903cd4fc26b1f898942a1da5066b2d4679833
Gayangi Seneviratne <sgayangi@gmail.com>
2021-09-08 12:22:25 +0530

    variable names updated

--- lib/ui/customer/current_orders/dummy_values.dart
+++ lib/ui/customer/current_orders/dummy_values.dart
@@ -2,25 +2,25 @@ import 'package:go_pharma/repos/customer/order/current_order_model.dart';
 
 List<CurrentOrder> currentOrders = [
   new CurrentOrder(
-    order_id: "#abcd1234",
+    orderId: "#abcd1234",
     price: 500.00,
     orderedDate: DateTime.now(),
     status: "Processing",
   ),
   new CurrentOrder(
-    order_id: "#abcd2345",
+    orderId: "#abcd2345",
     price: 500.00,
     orderedDate: DateTime.now(),
     status: "Packing",
   ),
   new CurrentOrder(
-    order_id: "#abcd3456",
+    orderId: "#abcd3456",
     price: 1000.00,
     orderedDate: DateTime.now(),
     status: "Delivered",
   ),
   new CurrentOrder(
-    order_id: "#abcd3456",
+    orderId: "#abcd3456",
     price: 1000.00,
     orderedDate: DateTime.now(),
     status: "In Transit",
--- lib/ui/customer/past_orders/dummy_values.dart
+++ lib/ui/customer/past_orders/dummy_values.dart
@@ -2,7 +2,7 @@ import 'package:go_pharma/repos/customer/order/past_order_model.dart';
 
 List<PastOrder> pastOrders = [
   new PastOrder(
-    order_id: "#abcd1234",
+    orderId: "#abcd1234",
     price: 500.00,
     completedDate: DateTime.now(),
     orderedDate: DateTime.now(),
