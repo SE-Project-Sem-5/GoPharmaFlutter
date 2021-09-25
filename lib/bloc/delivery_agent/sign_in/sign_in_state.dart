57a234d0e095282e4ffd2c2a208cb265bc005d0d
Gayangi Seneviratne <sgayangi@gmail.com>
2021-09-25 22:19:41 +0530

    sign in for delivery agent - 2fa added

--- lib/bloc/delivery_agent/sign_in/sign_in_state.dart
+++ lib/bloc/delivery_agent/sign_in/sign_in_state.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
+enum DeliveryAgentSignInStep {
+  DELIVERYAGENTSIGNINSTEP_START,
+  DELIVERYAGENTSIGNINSTEP_2FA
+}
+
 @immutable
 class DeliveryAgentSignInState {
   final String error;
   final bool isVisible;
+  final DeliveryAgentSignInStep step;
+  final String twoFA;
 
   DeliveryAgentSignInState({
-      this.error,
-      this.isVisible,
+    this.twoFA,
+    this.error,
+    this.step,
+    this.isVisible,
   });
 
   static DeliveryAgentSignInState get initialState => DeliveryAgentSignInState(
         error: '',
         isVisible: true,
+        step: DeliveryAgentSignInStep.DELIVERYAGENTSIGNINSTEP_START,
+        twoFA: '',
       );
 
   DeliveryAgentSignInState clone({
     String error = '',
     bool isVisible = true,
+    DeliveryAgentSignInStep step,
+    String twoFA,
   }) {
     return DeliveryAgentSignInState(
-      error: error,
-      isVisible: isVisible,
+      error: error ?? this.error,
+      isVisible: isVisible ?? this.isVisible,
+      step: step ?? this.step,
+      twoFA: twoFA ?? this.twoFA,
     );
   }
 }
