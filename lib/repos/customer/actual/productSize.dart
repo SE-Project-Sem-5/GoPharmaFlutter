262208797294d5af578a10a501d72c83bb67d183
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-12 20:52:21 +0530

    product size added

--- lib/repos/customer/actual/productSize.dart
+++ lib/repos/customer/actual/productSize.dart
@@ -1,32 +1,21 @@
 class ProductSize {
-  int id;
   String sizeName;
   String description;
-  String createdAt;
-  String updatedAt;
 
-  ProductSize(
-      {this.id,
-      this.sizeName,
-      this.description,
-      this.createdAt,
-      this.updatedAt});
+  ProductSize({
+    this.sizeName,
+    this.description,
+  });
 
   ProductSize.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
     sizeName = json['sizeName'];
     description = json['description'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
     data['sizeName'] = this.sizeName;
     data['description'] = this.description;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
     return data;
   }
 }
