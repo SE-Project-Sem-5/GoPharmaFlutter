db227a9170e72e519ab9990d684018d343feced9
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-13 12:25:25 +0530

    methods added

--- lib/repos/customer/actual/order/orderAPIProvider.dart
+++ lib/repos/customer/actual/order/orderAPIProvider.dart
@@ -5,10 +5,25 @@ import 'package:go_pharma/repos/customer/actual/order/order.dart';
 class OrderAPIProvider {
   final Dio _dio = Client.init();
 
-  Future<OrderList> getDeliveryChargeForNormalOrder(int customerID) async {
+  Future<OrderList> getProcessingOrders(int customerID) async {
     try {
       Response response = await _dio.post(
-        "customer/order/delivery",
+        "customer/order/processing/view",
+        data: {"customerID": customerID},
+      );
+      print(response);
+      print(response.data["data"]);
+      return OrderList.fromJson(response.data["data"]);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return OrderList();
+    }
+  }
+
+  Future<OrderList> getCompletedOrders(int customerID) async {
+    try {
+      Response response = await _dio.post(
+        "customer/order/delivered/view",
         data: {"customerID": customerID},
       );
       print(response);
