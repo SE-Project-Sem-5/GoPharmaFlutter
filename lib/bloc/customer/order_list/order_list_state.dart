f94d2fd3daf3739e41e4932eb35fa815a7309511
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-13 14:34:48 +0530

    loading added

--- lib/bloc/customer/order_list/order_list_state.dart
+++ lib/bloc/customer/order_list/order_list_state.dart
@@ -6,27 +6,32 @@ class OrderListState {
   final String error;
   final Map<String, OrderList> orderList;
   final String status;
+  final bool isLoading;
 
   OrderListState({
     this.error,
     this.status,
     this.orderList,
+    this.isLoading,
   });
 
   static OrderListState get initialState => OrderListState(
         error: '',
         status: '',
         orderList: {},
+        isLoading: false,
       );
 
   OrderListState clone({
     String error = '',
     Map<String, OrderList> orderList,
     String status,
+    bool isLoading,
   }) {
     return OrderListState(
       error: error ?? this.error,
       status: status ?? this.status,
+      isLoading: isLoading ?? this.isLoading,
       orderList: orderList ?? this.orderList,
     );
   }
