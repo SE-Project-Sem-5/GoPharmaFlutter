f9a56ce32508e38fdc40a430b93789a6faa761e8
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-14 22:04:15 +0530

    reserved

--- lib/ui/delivery_agent/reserved_deliveries/reserved_deliveries_page.dart
+++ lib/ui/delivery_agent/reserved_deliveries/reserved_deliveries_page.dart
@@ -3,19 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_pharma/bloc/delivery_agent/delivery_list/delivery_list_bloc.dart';
 import 'package:go_pharma/bloc/delivery_agent/delivery_list/delivery_list_state.dart';
 import 'package:go_pharma/ui/delivery_agent/common_skeleton.dart';
-import 'pending_delivery_card.dart';
+import 'reserved_delivery_card.dart';
 
 class PendingDeliveriesPage extends StatelessWidget {
-  //TODO:replace with the list gotten from the backend
-  static final String id = "pending_deliveries_page";
-  final String title = "Pending Deliveries";
+  static final String id = "reserved_deliveries_page";
+  final String title = "Reserved Deliveries";
 
   @override
   Widget build(BuildContext context) {
     final deliveryListBloc = BlocProvider.of<DeliveryListBloc>(context);
 
     return CommonSkeleton(
-      title: "Pending Deliveries",
+      title: title,
       child: BlocBuilder<DeliveryListBloc, DeliveryListState>(
         builder: (context, state) {
           return state.isLoading
