368527a82892a5c046b99d660320c98bc4c6c3b7
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-14 22:01:50 +0530

    common skeleton added

---
+++ lib/ui/delivery_agent/common_skeleton.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'drawer.dart';
+
+class CommonSkeleton extends StatelessWidget {
+  final Widget child;
+  final String title;
+  CommonSkeleton({@required this.child, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: child,
+        drawer: DeliveryAgentDrawer(),
+      ),
+    );
+  }
+}
