ff461c2a83b4d3ee9dfb07f5c13b855e6e7d1b5f
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-14 17:45:00 +0530

    bloc added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:responsive_framework/utils/scroll_behavior.dart';
 import 'bloc/customer/category/category_bloc.dart';
 import 'bloc/customer/checkout/checkout_bloc.dart';
+import 'bloc/delivery_agent/delivery_list/delivery_list_bloc.dart';
 import 'config/routes.dart';
 import 'config/theme.dart';
 
@@ -55,6 +56,9 @@ class GoPharmaApp extends StatelessWidget {
         ),
         BlocProvider<DeliveryAgentRootBloc>(
           create: (context) => DeliveryAgentRootBloc(context),
+        ),
+        BlocProvider<DeliveryListBloc>(
+          create: (context) => DeliveryListBloc(context),
         ),
       ],
       child: MaterialApp(
