991b2f0db488f8f8199df75fc3d646f12b3c23b0
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-15 22:38:41 +0530

    ui refactored

--- lib/ui/delivery_agent/reserved_deliveries/reserved_delivery_full_view.dart
+++ lib/ui/delivery_agent/reserved_deliveries/reserved_delivery_full_view.dart
@@ -10,143 +10,151 @@ class ReservedDeliveryFullView extends StatelessWidget {
   final double rightPadding = 30.0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            decoration: new BoxDecoration(
-              boxShadow: [
-                new BoxShadow(
-                  color: GoPharmaColors.PrimaryColor.withOpacity(0.2),
-                  blurRadius: 15.0,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Delivery Details"),
+        ),
+        body: Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: GoPharmaColors.PrimaryColor.withOpacity(0.2),
+                      blurRadius: 15.0,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: rightPadding,
-                    ),
-                    child: Text(
-                      "Delivery " + delivery.id.toString(),
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                  HorizontalLine(
-                    leftPadding: leftPadding,
-                    rightPadding: rightPadding,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: leftPadding),
-                    child: Text(
-                      'Reservation Details: ',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                  HorizontalLine(
-                    leftPadding: leftPadding,
-                    rightPadding: rightPadding,
-                  ),
-                  ListView.builder(
-                    physics: ClampingScrollPhysics(),
-                    itemCount: 1,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) => Padding(
-                      padding: EdgeInsets.only(left: leftPadding, top: 5),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Product: " + delivery.productName,
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "Quantity: " + delivery.quantity.toString(),
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "Supplier Name: " + delivery.supplierName,
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "Collection Point: " + delivery.address,
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "Delivery Point: " +
-                                      delivery.district +
-                                      " District Warehouse",
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                              ],
-                            ),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: rightPadding,
+                        ),
+                        child: Text(
+                          "Delivery " + delivery.id.toString(),
+                          style: TextStyle(
+                            fontSize: 20.0,
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: leftPadding,
-                      right: rightPadding,
-                      bottom: 15.0,
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: GoPharmaColors.PrimaryColor,
-                        textStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      child: Text(
-                        "Confirm Collection",
+                      HorizontalLine(
+                        leftPadding: leftPadding,
+                        rightPadding: rightPadding,
                       ),
-                    ),
+                      Padding(
+                        padding: EdgeInsets.only(left: leftPadding),
+                        child: Text(
+                          'Reservation Details: ',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                      HorizontalLine(
+                        leftPadding: leftPadding,
+                        rightPadding: rightPadding,
+                      ),
+                      ListView.builder(
+                        physics: ClampingScrollPhysics(),
+                        itemCount: 1,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => Padding(
+                          padding: EdgeInsets.only(left: leftPadding, top: 5),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Product: " + delivery.productName,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "Quantity: " +
+                                          delivery.quantity.toString(),
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "Supplier Name: " + delivery.supplierName,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "Collection Point: " + delivery.address,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "Delivery Point: " +
+                                          delivery.district +
+                                          " District Warehouse",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: leftPadding,
+                          right: rightPadding,
+                          bottom: 15.0,
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: GoPharmaColors.PrimaryColor,
+                            textStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          child: Text(
+                            "Confirm Collection",
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
