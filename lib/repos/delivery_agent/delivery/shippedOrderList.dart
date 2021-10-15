4957bc6c239029c85ed7717af73ead326c775057
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-15 19:47:55 +0530

    shipped deliveries state

---
+++ lib/repos/delivery_agent/delivery/shippedOrderList.dart
@@ -0,0 +1,89 @@
+class ShippedOrderList {
+  List<ShippedDelivery> deliveries;
+
+  ShippedOrderList({this.deliveries});
+
+  ShippedOrderList.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      deliveries = [];
+      json['data'].forEach((v) {
+        deliveries.add(new ShippedDelivery.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.deliveries != null) {
+      data['deliveries'] = this.deliveries.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class ShippedDelivery {
+  int orderID;
+  int totalPrice;
+  String destinationDistrict;
+  List<Products> products;
+
+  ShippedDelivery({
+    this.orderID,
+    this.totalPrice,
+    this.destinationDistrict,
+    this.products,
+  });
+
+  ShippedDelivery.fromJson(Map<String, dynamic> json) {
+    orderID = json['orderID'];
+    totalPrice = json['totalPrice'];
+    destinationDistrict = json['destinationDistrict'];
+    if (json['products'] != null) {
+      products = [];
+      json['products'].forEach((v) {
+        products.add(new Products.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['orderID'] = this.orderID;
+    data['totalPrice'] = this.totalPrice;
+    data['destinationDistrict'] = this.destinationDistrict;
+    if (this.products != null) {
+      data['products'] = this.products.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Products {
+  int id;
+  int quantity;
+  int totalPrice;
+  String productName;
+
+  Products({
+    this.id,
+    this.quantity,
+    this.totalPrice,
+    this.productName,
+  });
+
+  Products.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    quantity = json['quantity'];
+    totalPrice = json['totalPrice'];
+    productName = json['productName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['quantity'] = this.quantity;
+    data['totalPrice'] = this.totalPrice;
+    data['productName'] = this.productName;
+    return data;
+  }
+}
