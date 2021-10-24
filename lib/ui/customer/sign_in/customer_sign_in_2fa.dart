cb3079f31def62c9427f947c5a6783db40291a68
Gayangi Seneviratne <sgayangi@gmail.com>
2021-10-24 18:03:34 +0530

    authentication route completed

--- lib/ui/customer/sign_in/customer_sign_in_2fa.dart
+++ lib/ui/customer/sign_in/customer_sign_in_2fa.dart
@@ -5,6 +5,7 @@ import 'package:go_pharma/bloc/customer/customer_root/customer_root_event.dart';
 import 'package:go_pharma/bloc/customer/customer_root/customer_root_state.dart';
 import 'package:go_pharma/ui/common/colors.dart';
 import 'package:go_pharma/ui/common/widgets/rounded_button_filled.dart';
+import 'package:go_pharma/ui/customer/customer_home_page.dart';
 import 'package:pinput/pin_put/pin_put.dart';
 
 class CustomerSignIn2FA extends StatelessWidget {
@@ -24,81 +25,92 @@ class CustomerSignIn2FA extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<CustomerRootBloc>(context);
     String title = "Please enter your 6 digit code";
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Spacer(),
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: GoPharmaColors.PrimaryColor,
+    return BlocListener<CustomerRootBloc, CustomerRootState>(
+      listenWhen: (context, state) {
+        return state.signUpProcessState == SignUpProcessState.COMPLETED;
+      },
+      listener: (context, state) {
+        Navigator.pushReplacementNamed(
+          context,
+          CustomerHomePage.id,
+        );
+      },
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Spacer(),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: GoPharmaColors.PrimaryColor,
+              ),
             ),
-          ),
-          Spacer(),
-          BlocBuilder<CustomerRootBloc, CustomerRootState>(
-            builder: (context, state) {
-              _twoFAController.text = state.twoFA;
-              return Container(
-                margin: const EdgeInsets.all(20.0),
-                padding: const EdgeInsets.all(20.0),
-                child: PinPut(
-                  // ignore: missing_return
-                  validator: (value) {
-                    if (value == null || value == '') {
-                      return "Invalid code. Please try again.";
-                    }
-                  },
-                  fieldsCount: 6,
-                  onSubmit: (String pin) {
-                    print(pin);
-                  },
-                  focusNode: _pinPutFocusNode,
-                  controller: _twoFAController,
-                  submittedFieldDecoration: _pinPutDecoration.copyWith(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  selectedFieldDecoration: _pinPutDecoration,
-                  followingFieldDecoration: _pinPutDecoration.copyWith(
-                    borderRadius: BorderRadius.circular(5.0),
-                    border: Border.all(
-                      color: GoPharmaColors.PrimaryColor.withOpacity(.5),
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              BlocBuilder<CustomerRootBloc, CustomerRootState>(
-                builder: (context, state) {
-                  return RoundedButtonFilled(
-                    title: "Sign In",
-                    size: MediaQuery.of(context).size,
-                    fillColor: GoPharmaColors.PrimaryColor,
-                    textColor: GoPharmaColors.WhiteColor,
-                    onTapped: () {
-                      if (_formKey.currentState.validate()) {
-                        bloc.add(
-                          VerifyTwoFACode(
-                            twoFA: _twoFAController.text,
-                          ),
-                        );
+            Spacer(),
+            BlocBuilder<CustomerRootBloc, CustomerRootState>(
+              builder: (context, state) {
+                _twoFAController.text = state.twoFA;
+                return Container(
+                  margin: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(20.0),
+                  child: PinPut(
+                    // ignore: missing_return
+                    validator: (value) {
+                      if (value == null || value == '') {
+                        return "Invalid code. Please try again.";
                       }
                     },
-                  );
-                },
-              ),
-            ],
-          ),
-          Spacer(),
-        ],
+                    fieldsCount: 6,
+                    onSubmit: (String pin) {
+                      print(pin);
+                    },
+                    focusNode: _pinPutFocusNode,
+                    controller: _twoFAController,
+                    submittedFieldDecoration: _pinPutDecoration.copyWith(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    selectedFieldDecoration: _pinPutDecoration,
+                    followingFieldDecoration: _pinPutDecoration.copyWith(
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(
+                        color: GoPharmaColors.PrimaryColor.withOpacity(.5),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                BlocBuilder<CustomerRootBloc, CustomerRootState>(
+                  builder: (context, state) {
+                    return RoundedButtonFilled(
+                      title: "Sign In",
+                      size: MediaQuery.of(context).size,
+                      fillColor: GoPharmaColors.PrimaryColor,
+                      textColor: GoPharmaColors.WhiteColor,
+                      onTapped: () {
+                        if (_formKey.currentState.validate()) {
+                          bloc.add(
+                            VerifyTwoFACode(
+                              twoFA: _twoFAController.text,
+                            ),
+                          );
+                        }
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
